855a931ae4e439901e753083dd286050c5799ef5
Vishal Bollam <60523632+vishalbollam@users.noreply.github.com>
2020-12-02 10:45:09 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Recipe from './Recipe';
 
 const App = () => {
   const APP_ID = "b084a839 ";
   const APP_KEY = " 97caa9fe3f94d80774c36819432aad93 ";
 
+  const [recipes,setRecipes]=useState([]);
 
   useEffect(() => {
     getRecipes();
   }, []);
+
   const getRecipes= async()=>{ ;
   const response= await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
-  console.log(data);
+  setRecipes(data.hits);
 }
 
   return (
@@ -23,6 +26,11 @@ const App = () => {
           search
         </button>
       </form>
+      {recipes.map(recipe=>(
+        <Recipe 
+        key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories}
+        image={recipe.recipe.image}  />
+      ))}
     </div>
   );
 };
---
+++ src/Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Recipe =()=>{
+
+    return(
+<div>
+
+    <h1>title</h1>
+    <p> calories</p>
+    <img src='image' alt=""/>
+    </div>
+
+    );
+}
+export default Recipe();
